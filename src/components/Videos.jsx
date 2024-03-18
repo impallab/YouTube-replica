8b5e9f3ae84403103c0f4b374d33263e39218c9c
impallab <pallab13nayagram@gmail.com>
2024-03-18 22:10:59 +0530

    created .env files and set up api key , set up proxy and checked the response coming form the api

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 const Videos = () => {
-    return (
-        <div >Videos <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum rerum nam maxime a repellendus eos ducimus numquam deleniti commodi ipsam. Recusandae voluptate molestiae, deserunt tenetur alias corporis in exercitationem! Amet.</p></div>
-    )
-}
+    const fetchVideos = async () => {
+        try {
+            const videosData = await axios.get(`${import.meta.env.VITE_YOUTUBE_VIDEO_API_URL}+${import.meta.env.VITE_YOUTUBE_VIDEO_API_KEY}`);
+            console.log(videosData.data);
+        } catch (error) {
+            console.log("Failed to fetch data : ", error);
+        }
+    };
 
-export default Videos
+    useEffect(() => {
+        fetchVideos();
+    }, []);
+
+    return <div></div>;
+};
+
+export default Videos;
